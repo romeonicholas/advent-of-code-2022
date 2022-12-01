cb9823c6dbfa9ed3fba85f3a7623bff9c5a0a833
romeonicholas <79602065+romeonicholas@users.noreply.github.com>
2022-12-01 13:08:08 +0100

    Remove extraneous list

--- 1.12.2022/challenge1.py
+++ 1.12.2022/challenge1.py
@@ -5,19 +5,13 @@
 challenge_input = open("input1.txt", "r")
 data = challenge_input.read().splitlines()
 
-meals_by_elf = [[]]
-current_elf = meals_by_elf[0]
 greatest_calorie_count = 0
 current_calorie_count = 0
 
 for meal in data:
     if meal != '':
-        current_elf.append(meal)
         current_calorie_count += int(meal)
     else:
-        meals_by_elf.append([])
-        current_elf = meals_by_elf[-1]
-
         if current_calorie_count > greatest_calorie_count:
             greatest_calorie_count = current_calorie_count
 
